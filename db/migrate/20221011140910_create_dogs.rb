c7911a07391fd9b98f8dd00b9acc20ec84d29a30
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-11 17:10:39 +0300

    add dogs attributes

--- db/migrate/20221011140910_create_dogs.rb
+++ db/migrate/20221011140910_create_dogs.rb
@@ -1,6 +1,16 @@
 class CreateDogs < ActiveRecord::Migration[6.1]
   def change
     create_table :dogs do |t|
+          t.string :name
+          t.string :breed
+          t.string :image_url
+          t.string :height
+          t.string :weight
+          t.string :life_expectancy
+          t.string :family_life
+          t.string :physical
+          t.string :social_life
+          t.string :personality
 
       t.timestamps
     end
