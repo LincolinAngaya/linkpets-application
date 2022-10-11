eae44a151c11e1eb103ba86913140c3698e5c311
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-11 17:11:22 +0300

    add members attribuutes

--- db/migrate/20221011140827_create_admins.rb
+++ db/migrate/20221011140827_create_admins.rb
@@ -1,7 +1,14 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-
+      t.string :membership
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :location
+      t.string :image_url
+      t.string :password_digest
+      t.string :contact
       t.timestamps
     end
   end
