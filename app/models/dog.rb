86e3d8b7036262f1dea3af697b52cfd5438340ad
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 12:32:03 +0300

    remove the validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,5 @@
 class Dog < ApplicationRecord
     belongs_to :admin
-    validates :social_life, length: { maximum: 500 }
-    validates :family_life, length: { maximum: 500 }
-    validates :personality, length: { maximum: 500 }
-    validates :physical, length: { maximum: 500 }
+   
 
 end
