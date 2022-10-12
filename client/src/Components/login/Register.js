981dbdaa29723e1510623270444959bf69d6593d
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 15:48:40 +0300

    work on front-end functionality

--- client/src/Components/login/Register.js
+++ client/src/Components/login/Register.js
@@ -5,21 +5,30 @@ import "./design.css"
 
 const Register = () => {
   const history = useHistory();
-
+    const[membership, setMembership] = useState("");
     const[firstname, setFirstname] = useState("");
     const[lastname, setLastname] = useState("");
+    const[location, setLocation] = useState("");
+    const[image, setImage] = useState("");
+    const[contact, setContact] = useState("");
     const[email, setEmail] = useState("");
     const[password, setPassword] = useState("");  
 
 
     function handleOnChange(event){
-        if(event.target.name === "firstname"){
+      if(event.target.name === "membership"){
+            setMembership(event.target.value);
+        }else if(event.target.name === "firstname"){
             setFirstname(event.target.value);
-        }
-        else if (event.target.name === "lastname"){
+        }else if (event.target.name === "lastname"){
             setLastname(event.target.value);
-        }
-        else if (event.target.name === "email"){
+        }else if (event.target.name === "location"){
+            setLocation(event.target.value);
+        }else if (event.target.name === "image"){
+             setImage(event.target.value);
+        }else if (event.target.name === "contact"){
+             setContact(event.target.value);
+        }else if (event.target.name === "email"){
             setEmail(event.target.value);
         }
         else{
@@ -42,8 +51,11 @@ const Register = () => {
     function handleCreateAccount(event){
         event.preventDefault()
         const newAdmin = {
-            firstname: firstname,
-            lastname : lastname,
+            first_name: firstname,
+            last_name: lastname,
+            location: location,
+            image_url: image,
+            contact: contact,
             email: email.toLowerCase(), 
             password: password
         }
@@ -66,9 +78,13 @@ const Register = () => {
           <div className='sign-box'>
             <p>Don't have an account? Create your account, it takes less than a minute.</p>
             <form action='' onSubmit={handleCreateAccount}>
-              <input type='text' name='firstname'  placeholder='FirstName' value ={firstname} required id="firstname" onChange={handleOnChange} />
-              <input type='text' name='lastname' placeholder='LastName' value ={lastname} required id="lastname" onChange={handleOnChange} />
-              <input type='text' name='email' placeholder='Email' value ={email} required id="lastname" onChange={handleOnChange} />
+              <input type='text' name='membership'  placeholder='Membership' value ={membership} required id="membership" onChange={handleOnChange} />
+              <input type='text' name='firs_tname'  placeholder='FirstName' value ={firstname} required id="firstname" onChange={handleOnChange} />
+              <input type='text' name='last_name' placeholder='LastName' value ={lastname} required id="lastname" onChange={handleOnChange} />
+               <input type='text' name='location' placeholder='Location' value ={location} required id="location" onChange={handleOnChange} />
+              <input type='text' name='image_url'  placeholder='Image' value ={image} required id="image_url" onChange={handleOnChange} />
+              <input type='text' name='contact' placeholder='Contact' value ={contact} required id="contact" onChange={handleOnChange} />
+              <input type='text' name='email' placeholder='Email' value ={email} required id="email" onChange={handleOnChange} />
               <input type='password' name='password' placeholder='Password'value={password} required onChange={handleOnChange} />
               <button type='submit' className='primary-btn'>
                 Create an Account
