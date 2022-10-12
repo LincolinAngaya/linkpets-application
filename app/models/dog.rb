7c9f8ba6f39e1d6fbf98c7653d8ea4217ec11388
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 12:27:27 +0300

    add validations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,8 @@
 class Dog < ApplicationRecord
     belongs_to :admin
+    validates :social_life, length: { maximum: 500 }
+    validates :family_life, length: { maximum: 500 }
+    validates :personality, length: { maximum: 500 }
+    validates :physical, length: { maximum: 500 }
+
 end
