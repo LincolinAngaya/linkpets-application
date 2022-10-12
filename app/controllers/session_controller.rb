b1152925326f49b6204ed621f9dd5ed109332d38
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 10:11:11 +0300

    add login and log out routes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -16,25 +16,4 @@ class SessionController < ApplicationController
         head :no_content
     end
 
-    # def create
-    #     user = User.find_by(username: params[:username])
-    #     if user&.authenticate(params[:password])
-    #       session[:user_id] = user.id
-    #       render json: user, status: :created
-    #     else
-    #       render json: { error: "Invalid username or password" }, status: :unauthorized
-    #     end
-    #   end
-
-    # def create
-    #     admin = Admin.find_by(password: params[:password])
-    #     session[:admin_id] = admin.id
-    #     render json: admin
-    # end
-
-
-    # def destroy
-    #     session.delete :admin_id
-    #     head :no_content
-    # end
 end
