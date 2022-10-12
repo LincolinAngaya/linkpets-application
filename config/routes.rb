bb2744e8a70d1a64c42e336ae7ab874902dbdca4
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 11:14:33 +0300

    check on rutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   
   resources :dogs
-  resources :admins
+ # resources :admins
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  
+
+  post '/signup', to: 'admins#create'
+  get '/me', to: 'admins#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+ 
 end
