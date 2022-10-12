7f294b69d172e15a2c420ec7448baead03df409a
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 11:08:31 +0300

    add dog crud operations

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,4 +8,19 @@ class AdminsController < ApplicationController
           render json: { error: admin.errors.full_messages }, status: :unprocessable_entity
         end
       end
+
+      def show
+        admin = Admin.find_by(id: session[:admin_id])
+        if admin
+            render json: admin
+        else
+            render json: {error: "Unauthorized admin"}, status: :unauthorized
+        end
+    end
+
+      private
+    
+      def admin_params
+        params.permit(:membership, :first_name, :last_name, :email, :location, :image_url, :password, :contact)
+      end
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,31 @@
 class DogsController < ApplicationController
+    def index
+        if session[:admin_id]
+          render json: Dog.all, status: :created
+        else
+          render json: { errors: [] }, status: :unauthorized
+        end
+      end
+    
+      def create
+        # byebug
+        if session[:admin_id]
+          admin = Dog.find(session[:admin_id])
+          dog = admin.dogs.new(dog_params)
+          if dog.valid?
+            dog.save!
+            render json: dog, status: :created
+          else
+            render json: { errors: dog.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { errors: [] }, status: :unauthorized
+        end
+      end
+    
+      private
+    
+      def dog_params
+        params.permit(:name, :breed, :image_url, :height, :weight, :life_expectancy, :family_life, :physical, :social_life, :personality)
+      end
 end
