250cf0f45507f544ad47278a79d1e016de88390c
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 15:27:15 +0300

    modify the landing page

--- client/src/Components/HomeSection/Data.js
+++ client/src/Components/HomeSection/Data.js
@@ -1,26 +1,13 @@
-import slide1 from './slides/slider-1.jpg'
-import slide2 from './slides/slider-2.jpg'
 import slide3 from './slides/slider-3.jpg'
-import slide4 from './slides/slider-4.jpg'
 import slide5 from './slides/slider-5.jpg'
-import slide6 from './slides/slider-6.jpg'
+
 const Data = [{
-        image: `${slide1}`,
-    },
-    {
-        image: `${slide2}`,
+        image: `${slide5}`,
+
     },
     {
         image: `${slide3}`,
-    },
-    {
-        image: `${slide4}`,
-    },
-    {
-        image: `${slide5}`,
-    },
-    {
-        image: `${slide6}`,
-    },
+    }
+
 ]
 export default Data
