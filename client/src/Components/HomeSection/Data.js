74e1fda5e0e9e0f49a3a23e99740563d9c49751a
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 15:16:39 +0300

    change the image

--- client/src/Components/HomeSection/Data.js
+++ client/src/Components/HomeSection/Data.js
@@ -1,15 +1,26 @@
-import slide1 from './Works/img/slider-1.jpg'
-import slide2 from './Works/img/slider-2.jpg'
-import slide3 from './Works/img/slider-3.jpg'
-const Data = [
-  {
-    image: `${slide1}`,
-  },
-  {
-    image: `${slide2}`,
-  },
-  {
-    image: `${slide3}`,
-  },
+import slide1 from './slides/slider-1.jpg'
+import slide2 from './slides/slider-2.jpg'
+import slide3 from './slides/slider-3.jpg'
+import slide4 from './slides/slider-4.jpg'
+import slide5 from './slides/slider-5.jpg'
+import slide6 from './slides/slider-6.jpg'
+const Data = [{
+        image: `${slide1}`,
+    },
+    {
+        image: `${slide2}`,
+    },
+    {
+        image: `${slide3}`,
+    },
+    {
+        image: `${slide4}`,
+    },
+    {
+        image: `${slide5}`,
+    },
+    {
+        image: `${slide6}`,
+    },
 ]
 export default Data
