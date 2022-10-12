5070ee65d43d5caa3e8472b0d852ac3fa2e706c9
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-12 10:18:11 +0300

    create controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,11 @@
 class AdminsController < ApplicationController
+    def create
+        admin = Admin.create(admin_params)
+        if admin.valid?
+          session[:admin_id] = admin.id
+          render json: admin, status: :created
+        else
+          render json: { error: admin.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
 end
