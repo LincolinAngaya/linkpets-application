70c1ef2a36a75506c68f87cbdfcc8b70e7149051
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 22:07:00 +0300

    remove the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :dogs
+  
  # resources :admins
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  post '/signup', to: 'admins#create'
-  get '/me', to: 'admins#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
+ 
  
 end
