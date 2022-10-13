3dd4dee78ecfd1a21d09f516b7d41abac54a70c2
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 22:18:50 +0300

    add validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,8 @@
 class Pet < ApplicationRecord
+    validates :name, presence: true
+    validates :breed, presence: true
+     validates :height, presence: true 
+     validates :weight, presence: true
+
+    validates :body, social life: true, length: { minimum: 100 }
 end
