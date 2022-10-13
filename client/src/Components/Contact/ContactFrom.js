75872f74d07906613a065b094585bd08ac322286
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 10:19:26 +0300

    add input references

--- client/src/Components/Contact/ContactFrom.js
+++ client/src/Components/Contact/ContactFrom.js
@@ -3,37 +3,49 @@ import {useRef} from 'react'
 import "./Contact.css"
 
 const ContactFrom = (props) => {
-  const firstnameInputRef = useRef();
-    const lastnameInputRef = useRef();
-    const phoneInputRef = useRef();
-    const emailInputRef = useRef();
-    const subjectInputRef = useRef();
-    const companyInputRef = useRef();
-    const messageInputRef = useRef();
+     const nameInputRef = useRef();
+    const breedInputRef = useRef();
+    const image_urlInputRef = useRef();
+    const heightInputRef = useRef();
+    const weightInputRef = useRef();
+    const life_expectancyInputRef = useRef();
+    const family_lifeInputRef = useRef();
+    const socialInputRef = useRef();
+    const physicalInputRef = useRef();
+    const personalityInputRef = useRef();
+  
 
 
     function submitHandler(e){
       e.preventDefault();
 
-       const enteredFirstname = firstnameInputRef.current.value;
-       const enteredLastname = lastnameInputRef.current.value;
-       const enteredPhone = phoneInputRef.current.value;
-       const enteredEmail = emailInputRef.current.value;
-       const enteredSubject = subjectInputRef.current.value;
-       const enteredCompany = companyInputRef .current.value;
-       const enteredMessage = messageInputRef.current.value;
+       const enteredName = nameInputRef.current.value;
+       const enteredBreed = breedInputRef.current.value;
+       const enteredImage = image_urlInputRef.current.value;
+       const enteredHeight = heightInputRef.current.value;
+       const enteredWeight = weightInputRef.current.value;
+       const enteredLife_Expectancy = life_expectancyInputRef .current.value;
+       const enteredFamily_Life = family_lifeInputRef.current.value;
+       const enteredPhysical = physicalInputRef.current.value;
+       const enteredSocial_Life = socialInputRef.current.value;
+       const enteredPersonality = personalityInputRef .current.value;
+       
     
-       const contactDetails={
-           FirstName:enteredFirstname,
-           LastName:enteredLastname,
-           Phone:enteredPhone,
-           Email:enteredEmail,
-           Subject:enteredSubject,
-           Company:enteredCompany,
-           Message:enteredMessage,
+       const dogDetails={
+           name:enteredName,
+           breed:enteredBreed,
+           image_url:enteredImage,
+           height:enteredHeight,
+           weight:enteredWeight,
+           life_expectancy:enteredLife_Expectancy,
+           family_life:enteredFamily_Life,
+           Physical:enteredPhysical,
+           social_life:enteredSocial_Life,
+           personality:enteredPersonality,
+        
        };
       
-       props.onAddContact(contactDetails)
+       props.onAddDogs(dogDetails)
 }
  
  
@@ -51,58 +63,58 @@ const ContactFrom = (props) => {
                   <span>
                     Dog Name <label>*</label>
                   </span>
-                  <input type='text' name='fname' ref={firstnameInputRef} required />
+                  <input type='text' name='fname' ref={nameInputRef} required />
                 </div>
                 <div className='input'>
                   <span>
                     Dog Breed <label>*</label>
                   </span>
-                  <input type='text' name='lname' ref={lastnameInputRef} required />
+                  <input type='text' name='lname' ref={breedInputRef} required />
                 </div>
                 <div className='input'>
                   <span>
                     Dog Image <label>*</label>
                   </span>
-                  <input type='text' name='phone' ref={phoneInputRef} required  />
+                  <input type='text' name='phone' ref={image_urlInputRef} required  />
                 </div>
                 <div className='input'>
                   <span>
                     Height <label>*</label>
                   </span>
-                  <input type='email' name='email' ref={emailInputRef} required />
+                  <input type='email' name='email' ref={heightInputRef} required />
                 </div>
                 <div className='input'>
                   <span>Weight</span>
-                  <input type='text' name='subject' ref={subjectInputRef}  />
+                  <input type='text' name='subject' ref={weightInputRef}  />
                 </div>
                 <div className='input'>
                   <span>Life Expectancy</span>
-                  <input type='text' name='company' ref={companyInputRef}  />
+                  <input type='text' name='company' ref={life_expectancyInputRef}  />
                 </div>
               </div>
               <div className='input inputlast'>
                 <span>
                   Dog Family Life <label>*</label>
                 </span>
-                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+                <textarea cols='30' rows='5' name='message'ref={family_lifeInputRef} ></textarea>
               </div>
               <div className='input inputlast'>
                 <span>
                   Dog Physicality <label>*</label>
                 </span>
-                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+                <textarea cols='30' rows='5' name='message'ref={physicalInputRef} ></textarea>
               </div>
               <div className='input inputlast'>
                 <span>
                   Social Life <label>*</label>
                 </span>
-                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+                <textarea cols='30' rows='5' name='message'ref={socialInputRef} ></textarea>
               </div>
               <div className='input inputlast'>
                 <span>
                   Dog Personality <label>*</label>
                 </span>
-                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+                <textarea cols='30' rows='5' name='message'ref={personalityInputRef} ></textarea>
               </div>
               <button className='primary-btn'>Submit Now</button>
             </form>
