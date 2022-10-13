3bf7909e3f12b9137937d94671fece39f21a1a0f
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 22:15:57 +0300

    add pets params

--- db/migrate/20221013191017_create_pets.rb
+++ db/migrate/20221013191017_create_pets.rb
@@ -1,7 +1,16 @@
 class CreatePets < ActiveRecord::Migration[6.1]
   def change
     create_table :pets do |t|
-
+      t.string :name
+      t.string :breed
+      t.string :image_url
+      t.string :height
+      t.string :weight
+      t.string :life_expectancy
+      t.string :family_life
+      t.string :physical
+      t.string :social_life
+      t.string :personality
       t.timestamps
     end
   end
