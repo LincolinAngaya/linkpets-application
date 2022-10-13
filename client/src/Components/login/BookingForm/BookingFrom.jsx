ffc83575a22c4db260d848739dd1a05245d3c761
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 08:54:49 +0300

    add dog form

--- client/src/Components/login/BookingForm/BookingFrom.jsx
+++ client/src/Components/login/BookingForm/BookingFrom.jsx
@@ -1,33 +1,102 @@
+import React, {useState} from 'react'
+import { useHistory } from "react-router-dom";
+import HeadTitle from "../../Common/HeadTitle/HeadTitle"
+import "./design.css"
 
-import Booking from './Booking'
-import React from "react"
-import HeadTitle from '../../../Common/HeadTitle/HeadTitle';
-import { useHistory } from 'react-router-dom';
-
-function BookingFrom() {
-
+const Register = () => {
   const history = useHistory();
+    const[membership, setMembership] = useState("");
+    const[firstname, setFirstname] = useState("");
+    const[lastname, setLastname] = useState("");
+    const[location, setLocation] = useState("");
+    const[image, setImage] = useState("");
+    const[contact, setContact] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");  
 
-  function addBookingHandler(bookingDetails){
-    fetch('https://booking-666fa-default-rtdb.firebaseio.com/bookings.json',
-    {
-     method:'POST',
-     body:JSON.stringify(bookingDetails),
-     headers:{
-            'Content-Type':'application/json'
-     }
+
+    function handleOnChange(event){
+      if(event.target.name === "membership"){
+            setMembership(event.target.value);
+        }else if(event.target.name === "firstname"){
+            setFirstname(event.target.value);
+        }else if (event.target.name === "lastname"){
+            setLastname(event.target.value);
+        }else if (event.target.name === "location"){
+            setLocation(event.target.value);
+        }else if (event.target.name === "image"){
+             setImage(event.target.value);
+        }else if (event.target.name === "contact"){
+             setContact(event.target.value);
+        }else if (event.target.name === "email"){
+            setEmail(event.target.value);
+        }
+        else{
+            setPassword(event.target.value);
+        }  
+      }
+
+      function handleCreateAccountAlert(responseData = {}){
+        if(Object.values(responseData)[0] === "Admin already Exists"){
+            alert("Admin email already exists, please Login!")
+            history.replace("/sign-in")   
+        }
+        else{
+           alert("Account Created successfully!")
+           history.replace("/sign-in")     
+        }
     }
-    ).then(() => {
-      history.replace('/');
-    });
-}
+
+
+    function handleCreateAccount(event){
+        event.preventDefault()
+        const newAdmin = {
+            first_name: firstname,
+            last_name: lastname,
+            location: location,
+            image_url: image,
+            contact: contact,
+            email: email.toLowerCase(), 
+            password: password
+        }
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify(newAdmin)
+        })
+        .then(response => response.json())
+        .then(responseData => handleCreateAccountAlert(responseData));
+    }
+  
   return (
     <>
       <HeadTitle />
-      <Booking  onAddBooking={addBookingHandler}/>
-     
+      <section className='forms top'>
+        <div className='container'>
+          <div className='sign-box'>
+            <p>Don't have an account? Create your account, it takes less than a minute.</p>
+            <form action='' onSubmit={handleCreateAccount}>
+              <input type='text' name='membership'  placeholder='Membership' value ={membership} required id="membership" onChange={handleOnChange} />
+              <input type='text' name='firs_tname'  placeholder='FirstName' value ={firstname} required id="firstname" onChange={handleOnChange} />
+              <input type='text' name='last_name' placeholder='LastName' value ={lastname} required id="lastname" onChange={handleOnChange} />
+               <input type='text' name='location' placeholder='Location' value ={location} required id="location" onChange={handleOnChange} />
+              <input type='text' name='image_url'  placeholder='Image' value ={image} required id="image_url" onChange={handleOnChange} />
+              <input type='text' name='contact' placeholder='Contact' value ={contact} required id="contact" onChange={handleOnChange} />
+              <input type='text' name='email' placeholder='Email' value ={email} required id="email" onChange={handleOnChange} />
+              <input type='password' name='password' placeholder='Password'value={password} required onChange={handleOnChange} />
+              <button type='submit' className='primary-btn'>
+                Create an Account
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+
+      
     </>
   )
 }
 
-export default BookingFrom
+export default Register
