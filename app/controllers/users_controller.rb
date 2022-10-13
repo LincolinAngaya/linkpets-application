9c9329dfd6d88f4461d8f9614189d8ff64a1b4f8
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-14 01:18:41 +0300

    add params to the user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
     private
     def user_params
         
-        params.permit(:first_name, :last_name, :email,location, :password, :contact)
+        params.permit(:first_name, :last_name, :email, :location, :password, :contact)
     end
 end
