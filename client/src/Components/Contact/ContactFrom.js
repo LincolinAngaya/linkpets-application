3e1c9007fda12cc52339e452886ac556caed54f9
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 10:02:26 +0300

    modify the contacvt form to dog form

--- client/src/Components/Contact/ContactFrom.js
+++ client/src/Components/Contact/ContactFrom.js
@@ -42,78 +42,75 @@ const ContactFrom = (props) => {
       <section className='contact mtop'>
         <div className='container flex'>
           <div className='main-content'>
-            <h2>Contact From</h2>
-            <p>Fill out the form below, we will get back you soon.</p>
+            <p>Fill out the form below.</p>
 
             <form  onSubmit={submitHandler}>
               <div className='grid1'>
+               
                 <div className='input'>
                   <span>
-                    First Name <label>*</label>
+                    Dog Name <label>*</label>
                   </span>
                   <input type='text' name='fname' ref={firstnameInputRef} required />
                 </div>
                 <div className='input'>
                   <span>
-                    Last Name <label>*</label>
+                    Dog Breed <label>*</label>
                   </span>
                   <input type='text' name='lname' ref={lastnameInputRef} required />
                 </div>
                 <div className='input'>
                   <span>
-                    Phone Number <label>*</label>
+                    Dog Image <label>*</label>
                   </span>
                   <input type='text' name='phone' ref={phoneInputRef} required  />
                 </div>
                 <div className='input'>
                   <span>
-                    Email <label>*</label>
+                    Height <label>*</label>
                   </span>
                   <input type='email' name='email' ref={emailInputRef} required />
                 </div>
                 <div className='input'>
-                  <span>Subject</span>
+                  <span>Weight</span>
                   <input type='text' name='subject' ref={subjectInputRef}  />
                 </div>
                 <div className='input'>
-                  <span>Your Company</span>
+                  <span>Life Expectancy</span>
                   <input type='text' name='company' ref={companyInputRef}  />
                 </div>
               </div>
               <div className='input inputlast'>
                 <span>
-                  Write Your Message <label>*</label>
+                  Dog Family Life <label>*</label>
                 </span>
-                <textarea cols='30' rows='10' name='message'ref={messageInputRef} ></textarea>
+                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+              </div>
+              <div className='input inputlast'>
+                <span>
+                  Dog Physicality <label>*</label>
+                </span>
+                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+              </div>
+              <div className='input inputlast'>
+                <span>
+                  Social Life <label>*</label>
+                </span>
+                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
+              </div>
+              <div className='input inputlast'>
+                <span>
+                  Dog Personality <label>*</label>
+                </span>
+                <textarea cols='30' rows='5' name='message'ref={messageInputRef} ></textarea>
               </div>
               <button className='primary-btn'>Submit Now</button>
             </form>
           </div>
 
-          <div className='side-content'>
-            <h3>Visit our location</h3>
-            <p>we are located in the streets f nairobi along Kimathi roads in CBD.</p>
-            <br />
-
-            <h3>Message us</h3>
-            <span>info@guinness.com</span>
-            <br />
-            <span>+254705001954</span>
-            <br />
-
-            <div className='icon'>
-              <h3>Follow Us</h3>
-
-              <div className='flex_space'>
-                <i className='fab fa-facebook-f'></i>
-                <i className='fab fa-twitter'></i>
-                <i className='fab fa-linkedin'></i>
-                <i className='fab fa-instagram'></i>
-                <i className='fab fa-pinterest'></i>
-                <i className='fab fa-youtube'></i>
-              </div>
-            </div>
-          </div>
+        
+            
+         
         </div>
       </section>
 
