0b48b3baac541be9c015fb5f6ca13c664671a0ff
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-10-13 22:09:33 +0300

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   
+  resources :users
  # resources :admins
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
